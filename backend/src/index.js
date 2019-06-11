c89d058e83a615748963cc4d462cf19a6e17ca76
Victor Jordan <sistema.sgo.ccb@gmail.com>
2019-06-11 15:52:11 -0300

    Changing password

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://victorjordan95:viku1995@cluster0-y3luc.mongodb.net/test?retryWrites=true&w=majority`, {
+console.log(process.env.omnisenha);
+mongoose.connect(`mongodb+srv://victorjordan95:${process.env.omnisenha}@cluster0-y3luc.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true
 })
 
