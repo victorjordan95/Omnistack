a57af3e977e670c9f2e6fd14ee63d4642ad64ada
Victor Jordan <sistema.sgo.ccb@gmail.com>
2019-06-11 15:45:47 -0300

    separando os arquivos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen();
+mongoose.connect(`mongodb+srv://victorjordan95:viku1995@cluster0-y3luc.mongodb.net/test?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+})
+
+app.use(require('./routes'));
+
+app.listen(3333);
