7b5ab11b50a57e6f2ed3cb0b00f707e36c87c872
Victor Jordan <sistema.sgo.ccb@gmail.com>
2019-06-15 09:39:01 -0300

    adding changes for backend

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client'
 import { Text, View, Image, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
 
 import api from '../services/api';
@@ -24,12 +25,31 @@ export default class Feed extends Component {
     };
 
     async componentDidMount() {
-        // this.registerToSocket();
-        
         const response = await api.get('posts');
+        // this.registerToSocket();
         console.log(response.data)
         this.setState({ feed: response.data })
     }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333')
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed ]});
+        })
+        console.log(feed);
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post => post._id === likedPost._id ? likedPost : post)
+            });
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`)
+    }
+
 
     render() {
         return (
@@ -50,7 +70,7 @@ export default class Feed extends Component {
                             <Image style={styles.feedImage} source={{ uri: `http://localhost:3333/files/${item.image}` }} />
                             <View style={styles.FeedItemFooter}>
                                 <View style={styles.actions}>
-                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                    <TouchableOpacity style={styles.action} onPress={() => this.handleLike(item._id)}>
                                         <Image source={like}/>
                                     </TouchableOpacity>
                                     <TouchableOpacity style={styles.action} onPress={() => {}}>
@@ -62,8 +82,8 @@ export default class Feed extends Component {
                                 </View>
 
                                 <Text style={styles.likes}>{item.likes} curtidas</Text>
-                                <Text style={styles.description}>{item.description} curtidas</Text>
-                                <Text style={styles.hashtags}>{item.hashtags} curtidas</Text>
+                                <Text style={styles.description}>{item.description} </Text>
+                                <Text style={styles.hashtags}>{item.hashtags} </Text>
                             </View>
                         </View>
                     )}
